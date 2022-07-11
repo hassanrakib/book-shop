bec1f2b9b4e4493aae7e706e5cb5589b50309672
Rakib Hassan <mailrakibhassan@gmail.com>
2022-07-11 10:50:29 +0600

    removed search btn

--- src/Pages/Home/Search/Search.js
+++ src/Pages/Home/Search/Search.js
@@ -7,7 +7,11 @@ export default class Search extends React.Component {
     const { searchText, handleInputChange, searchLoading } = this.props;
     return (
       <div className="flex items-center border border-blue-custom rounded bg-slate-100 pl-2 w-2/5 mx-auto my-20">
-        <img className="h-6 w-6" src={searchIcon} alt="search-icon" />
+        {searchLoading ? (
+          <Loading isLarge={false} height="auto" width="w-6" />
+        ) : (
+          <img className="h-6 w-6" src={searchIcon} alt="search-icon" />
+        )}
         <input
           className="w-full bg-transparent outline-none placeholder:text-slate-400 px-3 py-4"
           type="search"
@@ -17,14 +21,6 @@ export default class Search extends React.Component {
           onChange={handleInputChange}
           placeholder="Search Book"
         />
-        {/* bg-blue-custom removed */}
-        <button className="rounded-r text-blue-custom px-9 py-4 inline-block">
-          {searchLoading ? (
-            <Loading isLarge={false} height="h-5" width="w-5" />
-          ) : (
-            "Search"
-          )}
-        </button>
       </div>
     );
   }
